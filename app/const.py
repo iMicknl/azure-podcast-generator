a22953e388aafb4322739c8ecce1be8f7f603e9f
Mick Vleeshouwer <mick@imick.nl>
2024-10-22 14:08:35 +0000

    Add new HD voice Brian

--- app/const.py
+++ app/const.py
@@ -14,6 +14,7 @@ AZURE_HD_VOICES = {
     "Andrew": "en-US-Andrew2:DragonHDLatestNeural",
     "Aria": "en-US-Aria:DragonHDLatestNeural",
     "Ava": "en-US-Ava:DragonHDLatestNeural",
+    "Brian": "en-US-Brian:DragonHDLatestNeural",
     "Davis": "en-US-Davis:DragonHDLatestNeural",
     # "Emma": "en-US-Emma:DragonHDLatestNeural",
     "Emma": "en-US-Emma2:DragonHDLatestNeural",
